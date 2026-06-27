4dacd87aec97c7d8bc26e13361753b179ab31584
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-06-28 04:42:36 +0800

    feat: redirect users to landing and dashboard dynamically

---
+++ inc/helpers.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+function is_logged_in(): bool
+{
+    return true;
+}
--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-?>
+
+declare(strict_types=1);
+
+require_once "inc/helpers.php";
+
+if (is_logged_in()) {
+    header("Location: dashboard.php");
+    exit();
+}
+
+header("Location: landing.php");
+exit();
