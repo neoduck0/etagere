5918cdd494a3e9f741fac26245f645f20f74557e
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-06-27 19:59:14 +0800

    feat: handle sql connection silently

--- inc/db.php
+++ inc/db.php
@@ -8,11 +8,9 @@ $conn = "";
 try {
     $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
-    if ($conn) {
-        echo "Connected to database";
-    } else {
-        echo "Failed to connect to database";
+    if (!$conn) {
+        $conn = null;
     }
 } catch (mysqli_sql_exception) {
-    echo "Failed to connect to database";
+    $conn = null;
 }
