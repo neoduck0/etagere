c537274f1ac7c9b2434bca60396d08c5a2371224
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-06-29 18:45:51 +0800

    refactor: make index.php be the dashboard

--- index.php
+++ index.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 require_once "inc/helpers.php";
 
-if (is_logged_in()) {
-    header("Location: dashboard.php");
+if (is_logged_in() === false) {
+    header("Location: landing.php");
     exit();
 }
-
-header("Location: landing.php");
-exit();
+?>
